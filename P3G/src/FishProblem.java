5e507870b702e27453f1ac142d202d5a5e1cbc4d
Daniel Nucci <djnucci@hotmail.com>
2016-03-31 14:59:55 -0400

    Fishy buisness

---
+++ P3G/src/FishProblem.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FishProblem {
+
+	public static void main(String[] args) {
+	
+		Scanner read = new Scanner(System.in);
+		
+		int tankOne = read.nextInt();
+		int tankTwo = read.nextInt();
+		int tankThree = read.nextInt();
+		
+		if(tankOne > tankTwo){
+			
+			
+		} 
+		
+		
+
+	}
+
+}
