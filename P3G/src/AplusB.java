008e506f064f7d47e1ec86b754e477ac82da267c
Daniel Nucci <djnucci@hotmail.com>
2016-03-31 14:42:29 -0400

    testing

---
+++ P3G/src/AplusB.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AplusB {
+
+	public static void main(String[] args) {
+
+		Scanner read = new Scanner(System.in);
+
+		int a = read.nextInt();
+		int b = read.nextInt();
+
+		System.out.println(a + b);
+
+	}
+
+}
